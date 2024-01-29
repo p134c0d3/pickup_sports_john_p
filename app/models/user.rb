c08369c0ce6cb6bf6211e00bd552ebd794b159fa
p134c0d3 <70922864+p134c0d3@users.noreply.github.com>
2024-01-28 23:33:59 -0600

    Associations, Rails Console, Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 30}
+  validate :validate_username
   validates :email, presence: true, uniqueness: true, length: {minimum: 5, maximum: 255}, format: {
     with: URI::MailTo::EMAIL_REGEXP
   }
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  # associations
+  has_many :posts
+  has_one :profile
+
+  private
+  def  validate_username
+    unless username =~ /\A[a-zA-Z0-9_]+\Z/
+      errors.add(:username, "Can only contain letters, numbers, and underscores, and must contain at leaset one letter or number")
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_29_040112) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_29_052722) do
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
@@ -20,4 +36,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_040112) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
+  add_foreign_key "profiles", "users"
 end
